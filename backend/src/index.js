bf0e8b6b0473509288648dce81677adfaa0e5f66
Ashley <z5258869@ad.unsw.edu.au>
2021-09-28 22:28:52 +1000

    Removed swagger documentation as the node version for Vlab is to old for the package

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,5 @@
 import express from "express";
 import cors from 'cors';
-import swaggerUI from 'swagger-ui-express';
-import { swaggerDocs } from "./docs";
 
 // Make the server instance
 const app = express();
@@ -20,38 +18,12 @@ app.use(cors());
 // Middleware to parse JSON
 app.use(express.json())
 
-// Middleware used to generate automatic REST API documentation
-app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
-
-//////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////
-//                  Endpoint definitons below                   //
-//////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////
-
 // Makes a GET endpoint for the server
 app.get('/', (req, res) => {
   res.status(200).send('Mockup for Stockup');
 })
 
 // Makes a POST endpoint for the server
-// The @swagger comment is used by the automatic documentation
-// generator
-/**
- * @swagger
- * /dummy:
- *   post:
- *     description: Dummy endpoint accepting post requests
- *     parameters:
- *      - name: dummyParam
- *        description: title of the book
- *        in: body
- *        required: true
- *        type: object
- *     responses:
- *       200:
- *         description: Success
- */
 app.post('/dummy', (req, res) => {
   // Get the post parameter
   const param = req.body.myParam;
