6371116f54a3adbea27848aa1f2080ce9f3d9825
Mison <mison.caamwang@hotmail.com>
2021-10-04 15:41:19 +1100

    added login page on the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from "./comp/Login";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'; 
+import Dashboard from "./comp/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Login/>
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard/>
+          </Route>
+        </Switch>  
+      </div>
+    </Router>
   );
 }
 
