38ed07bf425cbe16c08bb74b0b2560da27ebd300
Ashley <ashleycheung2412@gmail.com>
2021-10-04 20:29:15 +1100

    Implemented api calls for the login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,25 @@
 import Login from "./comp/Login";
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'; 
 import Dashboard from "./comp/Dashboard";
+import API, { ApiContext } from "./api";
 
 function App() {
+  const api = new API();
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route exact path="/">
-            <Login/>
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard/>
-          </Route>
-        </Switch>  
-      </div>
-    </Router>
+    <ApiContext.Provider value={api}>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path="/">
+              <Login/>
+            </Route>
+            <Route path="/dashboard">
+              <Dashboard/>
+            </Route>
+          </Switch>  
+        </div>
+      </Router>
+    </ApiContext.Provider>
   );
 }
 
--- frontend/src/comp/Login.jsx
+++ frontend/src/comp/Login.jsx
@@ -1,8 +1,24 @@
 import { Grid,Paper, Avatar, TextField, Button, Typography,Link } from '@material-ui/core'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { useContext, useState } from 'react';
+import { ApiContext } from '../api';
 
 function Login() {
-
+    const api = useContext(ApiContext);
+    const [ username, setUsername ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const onLogIn = async () => {
+        const resp = await api.authLogin(username, password);
+        if (resp.status === 403) {
+            alert('Invalid username and password combination')
+        } else if (resp.status === 200) {
+            const jsondata = await resp.json();
+            alert('Log in successful')
+            console.log(jsondata);
+        } else {
+            alert(`Server returned unexpected status code of ${resp.status}`);
+        }
+    }
     const paperStyle={padding :20,height:'70vh',width:280, margin:"20px auto"}
     const avatarStyle={backgroundColor:'#1bbd7e'}
     const btnstyle={margin:'8px 0'}
@@ -13,10 +29,16 @@ function Login() {
                      <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
                     <h2>Log In</h2>
                 </Grid>
-                <TextField label='Username' placeholder='Enter username' fullWidth required/>
-                <TextField label='Password' placeholder='Enter password' type='password' fullWidth required/>
+                <TextField 
+                    value = {username} onChange={e => setUsername(e.target.value)}
+                    label='Username' placeholder='Enter username' fullWidth required/>
+                <TextField
+                    value = {password} onChange={e => setPassword(e.target.value)}
+                    label='Password' placeholder='Enter password' type='password' fullWidth required/>
              
-                <Button type='submit' color='primary' variant="contained" style={btnstyle} fullWidth>Log in</Button>
+                <Button 
+                    onClick = {onLogIn}
+                    type='submit' color='primary' variant="contained" style={btnstyle} fullWidth>Log in</Button>
                 <Typography >
                      <Link href="#" >
                         Forgot password ?
