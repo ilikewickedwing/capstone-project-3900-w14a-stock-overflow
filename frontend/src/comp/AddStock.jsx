6568058d3dae2d6a735b271f83ef564391796de0
Lily Cheung <lily.cheung35@gmail.com>
2021-10-12 09:58:34 -0400

    implement the search; yet to display it on frontend

--- frontend/src/comp/AddStock.jsx
+++ frontend/src/comp/AddStock.jsx
@@ -12,6 +12,56 @@ import {TextInput} from "../styles/styling";
 const AddStock = () => {
     const api = React.useContext(ApiContext);
     const [search, setSearch ] = React.useState("");
+    const [success, setSuccess] = React.useState(0);
+    const [queryRes, setRes] = React.useState([]);
+
+    var request = require('request');
+
+    // replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+    var url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${search}&apikey=59SO8FIM49NYQS21`;
+
+    const searchBar = async(e) =>{
+        e.preventDefault(); 
+        setSearch(e.target.value);
+        request.get({
+            url: url,
+            json: true,
+            headers: {'User-Agent': 'request'}
+        }, (err, res, data) => {
+            if (err) {
+            console.log('Error:', err);
+            } else if (res.statusCode !== 200) {
+            console.log('Status:', res.statusCode);
+            } else {
+            // data is successfully parsed as a JSON object:
+            const response =  data.bestMatches;
+
+            if (response){
+                const newList = []; 
+                response.forEach((obj) => {
+                    newList.push({
+                        code: obj["1. symbol"],
+                        name: obj["2. name"]
+                    });
+                })
+                setRes(newList);
+            }
+            }
+        });
+    }
+    const toggleSuccess = (num) => {
+        setSuccess(num);
+    }
+
+    // TODO 
+    // check successful search 
+    const checkSuccess = () => {
+        if (success === 1) {
+            // use the code and name, from the currently selected option
+            // to add the stock 
+            toggleSuccess(0);
+        }
+    }
 
     return (
         <ExpansionPanel>
@@ -19,7 +69,11 @@ const AddStock = () => {
           <u>  + Add New Stock </u> 
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
-            <TextInput required id="search" variant="outlined" label="Search Stock" onChange={(e) => setSearch(e.target.value) }/>
+        <form onSubmit={checkSuccess}>
+            <TextInput required id="search" variant="outlined" label="Search Stock" onChange={searchBar}/>
+        </form> 
+        
+            
         </ExpansionPanelDetails>
       </ExpansionPanel>
     )
