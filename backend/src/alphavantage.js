5c4a7aed8b89a422056ac5d853240f8eee2244f4
Richard <richardnmo@gmail.com>
2021-10-27 16:03:44 +1100

    Began implementing stock get functionality

--- backend/src/alphavantage.js
+++ backend/src/alphavantage.js
@@ -5,6 +5,8 @@ export class Alphavantage {
   constructor() {
     // Stores the last cached stocks
     this.cachedStocks = null;
+    // Stores the full information of stocks
+    this.infoCache = [];
     // How long in millisecond before calling get all stocks again
     const pollingInterval = 60000;
     this.intervalObj = setInterval(() => {this._getAllStocks()}, pollingInterval);
@@ -45,6 +47,59 @@ export class Alphavantage {
     this.cachedStocks = stocks;
     return stocks;
   }
+
+  async getStock(stock) {
+    console.log(this.infoCache);
+    // Search for stock in cache
+    const search = this.infoCache.filter(o => o.symbol === stock);
+    const time = Date.now();
+
+    if (search.length !== 0) {
+      console.log(search[0].time);
+      console.log(time - search[0].time);
+    }
+    if (search.length !== 0 && time - search[0].time < 600000) {
+      console.log("returning cached stock");
+      return search;
+    }
+
+    console.log("fetching cache");
+    // Fetch stock and add to cache
+    const resp = await this._getStock(stock);
+    return resp;
+  }
+
+  async _getStock(stock) {
+    const dailyRequest = await axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stock}&apikey=${apikey}`);
+    const weeklyRequest = await axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=${stock}&apikey=${apikey}`);
+
+
+    const priceRequest = await axios.get(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stock}&apikey=${apikey}`);     
+    const infoRequest = await axios.get(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${stock}&apikey=${apikey}`); 
+    
+    //console.log(await dailyRequest.data);
+
+    const time = new Date();
+    const obj = {
+      symbol: 'IBM',
+      info: {
+        daily: await dailyRequest.data,
+        weekly: await weeklyRequest.data,
+        price: await priceRequest.data,
+        info: await infoRequest.data,
+      },
+      time: time
+    }
+    
+    this.infoCache.push(obj);
+
+    return obj;
+  }
+
+  checkStock(check, against) {
+    return check == against;
+  }
+
   // Call this when deleting this object to remove all time intervals
   // to prevent a memory leak
   destroy() {
