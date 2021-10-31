b76ae226b3b86e80b06e9d73715c0414cd380a93
Ashley <ashleycheung2412@gmail.com>
2021-11-01 10:14:14 +1100

    Ohlc fiinished

--- frontend/src/graph/Ohlc.jsx
+++ frontend/src/graph/Ohlc.jsx
@@ -2,60 +2,56 @@ import React from "react";
 import { GRAPHCOLORS } from "./StocksGraph";
 
 /**
- * Candle stick shape
- * This is referenced from this code: https://codesandbox.io/s/8m6n8
+ * Ohlc shape
  */
 export default function Ohlc(props) {
   console.log(props);
   const [ high, low ] = props.highLow;
   const isIncreasing = props.open < props.close;
-  // const drawTopBottomLines = () => {
-  //   const heightRatio = Math.abs(props.height / (open - close));
-  //   if (Number.isNaN(heightRatio) || !Number.isFinite(heightRatio)) {
-  //     return null;
-  //   }
-  //   return (
-  //     <React.Fragment>
-  //       {/* bottom line */}
-  //       {isIncreasing ? (
-  //         <path
-  //           d={`
-  //             M ${props.x + props.width / 2}, ${props.y + props.height}
-  //             v ${(open - props.low) * heightRatio}
-  //           `}
-  //         />
-  //       ) : (
-  //         <path
-  //           d={`
-  //             M ${props.x + props.width / 2}, ${props.y}
-  //             v ${(close - props.low) * heightRatio}
-  //           `}
-  //         />
-  //       )}
-  //       {/* top line */}
-  //       {isIncreasing ? (
-  //         <path
-  //           d={`
-  //             M ${props.x + props.width / 2}, ${props.y}
-  //             v ${(close - props.high) * heightRatio}
-  //           `}
-  //         />
-  //       ) : (
-  //         <path
-  //           d={`
-  //             M ${props.x + props.width / 2}, ${props.y + props.height}
-  //             v ${(open - props.high) * heightRatio}
-  //           `}
-  //         />
-  //       )}
-  //     </React.Fragment>
-  //   )
-  // }
-  const color = isIncreasing ? GRAPHCOLORS.INCREASING : GRAPHCOLORS.DECREASING;
   const xIncrem = props.width / 3;
+  const drawOpenCloseLines = () => {
+    const heightRatio = Math.abs(props.height / (high - low));
+    if (Number.isNaN(heightRatio) || !Number.isFinite(heightRatio)) {
+      return null;
+    }
+    return (
+      <React.Fragment>
+        {/* open line */}
+        {/* {isIncreasing ? (
+          <path
+            d={`
+              M ${props.x + props.width / 2}, ${props.y + props.height}
+              v ${(open - props.low) * heightRatio}
+            `}
+          />
+        ) : (
+          <path
+            d={`
+              M ${props.x + props.width / 2}, ${props.y}
+              v ${(close - props.low) * heightRatio}
+            `}
+          />
+        )} */}
+        <path
+            d={`
+              M ${props.x}, ${props.y + (props.open - high) * heightRatio}
+              h ${xIncrem}
+            `}
+          />
+        {/* close line */}
+        <path
+            d={`
+              M ${props.x + props.width}, ${props.y + (props.close - high) * heightRatio}
+              h ${-1 * xIncrem}
+            `}
+          />
+      </React.Fragment>
+    )
+  }
+  const color = isIncreasing ? GRAPHCOLORS.INCREASING : GRAPHCOLORS.DECREASING;
   return (
     <g
-      stroke={color} fill={color} strokeWidth="2">
+      stroke={color} fill={color} strokeWidth="4">
       <path
         d={`
           M ${props.x + xIncrem},${props.y}
@@ -65,7 +61,7 @@ export default function Ohlc(props) {
           L ${props.x + xIncrem},${props.y}
         `}
       />
-      {/* { drawTopBottomLines() } */}
+      { drawOpenCloseLines() }
     </g>
   );
 }
