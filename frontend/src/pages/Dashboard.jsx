f9c23960a7f35f14f3295b7b8f1ba9fb7e8d4fe3
dollalilz <lily.cheung35@gmail.com>
2021-11-12 22:32:22 +1100

    more cards

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -28,13 +28,13 @@ export default function Dashboard() {
               </LeftBody>
               <RightBody elevation={10}>
                 <RightCard elevation={5}>
+                  <h3 style={{textAlign:'center'}}>Top Performer Amongst Friends</h3>
+                </RightCard>
+                <RightCard elevation={5}>
                 <div style={{textAlign:'center'}}>
                     <img style={{height:"auto", width:"50px"}} src={leaderboard} alt="leaderboard icon"/>
                   </div>
                   <h3 style={{textAlign:'center'}}>Global Rankings</h3>
-                </RightCard>
-                <RightCard elevation={5}>
-                  2nd card
                 </RightCard>
                 <RightCard elevation={5}>
                   3rd
