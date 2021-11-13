117378a82d1c853f82c7709e9f2ac973e8bd197b
dollalilz <lily.cheung35@gmail.com>
2021-11-14 03:48:03 +1100

    ?? why does the table not render sometimes????

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,9 @@
 import * as React from 'react';
+// component imports
+import Navigation from '../comp/Navigation';
+import Tabs from '../comp/Tabs';
+import PerformanceTable from '../comp/PerformanceTable/PerformanceTable';
+
 
 // styling imports 
 import { 
@@ -11,29 +16,23 @@ import {
   PfBar,
 } from '../styles/styling';
 import leaderboard from '../assets/leaderboard.png';
-
-// component imports
-import Navigation from '../comp/Navigation';
-import Tabs from '../comp/Tabs';
-import axios from 'axios';
-import { apiBaseUrl } from '../comp/const';
-
+import star from '../assets/star.png';
 
 export default function Dashboard() {
-  // first load render 
-    React.useEffect(() => {   
-      getGlobalRanks();
+  
+    // first load render 
+    React.useEffect(() => {  
     },[]);
-      
-  const getGlobalRanks = async () => {
-    try {
-      const request = axios.get(`${apiBaseUrl}/rankings/global`);
-      console.log(request);
-    } catch (e) {
-      alert(`Status Code ${e.response.status} : ${e.response.data.error}`,'error');
-    }
-  }
 
+  // const getGlobalRanks = async () => {
+  //   try {
+  //     const request = axios.get(`${apiBaseUrl}/rankings/global`);
+  //     console.log(request);
+  //   } catch (e) {
+  //     alert(`Status Code ${e.response.status} : ${e.response.data.error}`,'error');
+  //   }
+  // }
+  
   return (
     <PageBody className="font-two">
       <Navigation />
@@ -43,15 +42,20 @@ export default function Dashboard() {
               <PfBar>
                 <Heading>Dashboard</Heading> 
               </PfBar>
+                <h3>Overall Performance</h3>
+                <PerformanceTable />
               </LeftBody>
               <RightBody elevation={10}>
                 <RightCard elevation={5}>
+                  <div style={{textAlign:'center'}}>
+                    <img style={{height:"auto", width:"50px"}} src={star} alt="star icon"/>
+                  </div>
                   <h3 style={{textAlign:'center'}}>Today's Top Performer</h3>
                 </RightCard>
                 <RightCard elevation={5}>
                 <div style={{textAlign:'center'}}>
-                    <img style={{height:"auto", width:"50px"}} src={leaderboard} alt="leaderboard icon"/>
-                  </div>
+                  <img style={{height:"auto", width:"50px"}} src={leaderboard} alt="leaderboard icon"/>
+                </div>
                   <h3 style={{textAlign:'center'}}>Global Rankings</h3>
                 </RightCard>
                 <RightCard elevation={5}>
