c8b873b059d100c5d9cb59153ee4536062bdd4f7
dollalilz <lily.cheung35@gmail.com>
2021-11-15 22:13:46 +1100

    Co-authored-by: dragonalxd <dragonalxd@users.noreply.github.com>

--- frontend/src/pages/Friend.jsx
+++ frontend/src/pages/Friend.jsx
@@ -13,8 +13,6 @@ import {
 import Button from '@mui/material/Button';
 import Navigation from '../comp/Navigation';
 import Tabs from '../comp/Tabs';
-import RankTable from '../comp/RankTable'; 
-import leaderboard from '../assets/leaderboard.png';
 import { AlertContext } from '../App';
 import { useHistory, useParams } from 'react-router-dom';
 import axios from "axios";
@@ -22,19 +20,11 @@ import { apiBaseUrl } from '../comp/const';
 import FriendTab from '../comp/FriendTab';
 import PfTable from '../comp/PfTable';
 
-// stub data for rankings
-function createData(name, performance, rank) {
-  return { name, performance, rank };
-}
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 
-const rows = [
-  createData('richard_mo', '+400', 1),
-  createData('elon_musk', '+150', 2),
-  createData('user1', "+100", 3),
-  createData('dragonalxd', "+59", 4),
-];
-
-const myRanking = createData('dollalilz', "-300%", 99999);
 
 // note: friend is inclusive of celebrity profiles except celebrities are public profiles while friends are private 
 export default function Friend() {
@@ -56,7 +46,6 @@ export default function Friend() {
     const [tab, setTab] = React.useState('Summary');
     const [stocks, setStocks] = React.useState([]);
     const [selected, setGraphSelected] = React.useState([]);
-    const [rankings, setRankings] = React.useState(rows);
 
       // on first load 
     React.useEffect(() => {   
@@ -66,7 +55,6 @@ export default function Friend() {
     // load based on friendUId getting awaited 
     React.useEffect(() => {   
       loadPortfolios();
-      getRanking();
     },[friendUid]);
 
   
@@ -114,22 +102,6 @@ export default function Friend() {
       }
     }
 
-    const getRanking = async () => {
-      try {
-        const resp = await axios.get(`${apiBaseUrl}/rankings/friends?token=${token}`);
-        console.log(resp);
-        let list = [];
-        for (let i=0; i< resp.data.length; i++){
-          list.push(createData(resp.data[i].name, resp.data[i].performance, resp.data[i].rank));
-        }
-        setRankings(list);
-
-        // todo set ranking of the new thing 
-      } catch (e) {
-        alert(`Status Code ${e.response.status} : ${e.response.data.error}`,'error');
-      }
-    }
-
   return (
     <PageBody className="font-two">
       <Navigation />
@@ -141,14 +113,17 @@ export default function Friend() {
                 <PfBar>
                   <Heading>{handle} </Heading>
                 </PfBar>
+                  {portData.map((e) =>(
+                    <Tab key={e.name} label={e.name} onClick={setStocks(e.stocks)} />
+                  ))}
                   {portData.map((e)=>(
-                    <FriendTab
-                      key={e.name}
-                      name={e.name}
-                      stocks={e.stocks}
-                      setTab={setTab} 
-                      setStocks={setStocks}
-                    / >
+                      <FriendTab
+                        key={e.name}
+                        name={e.name}
+                        stocks={e.stocks}
+                        setTab={setTab} 
+                        setStocks={setStocks}
+                      / >
                   ))}
                   < br/> 
                   <h3>{tab}</h3>
@@ -162,16 +137,6 @@ export default function Friend() {
                   )}
               </LeftBody>
               <RightBody elevation={10}>
-                <RightCard elevation={5}>
-                  <div style={{textAlign:'center'}}>
-                    <img style={{height:"auto", width:"50px"}} src={leaderboard} alt="leaderboard icon"/>
-                  </div>
-                  <h3 style={{textAlign:'center'}}>Ranking amongst friends</h3>
-                  <RankTable
-                    rows={rankings}
-                    myRanking={myRanking}
-                  />
-                </RightCard>
                 <RightCard elevation={5}>
                   <h3 style={{textAlign:'center'}}>{handle}'s Activity</h3>
                 </RightCard>
